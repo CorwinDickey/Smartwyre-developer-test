d8715ad0bc6edbde3ec3d511e8d4375b317b1ec7
Corwin Dickey <62188881+CorwinDickey@users.noreply.github.com>
2025-02-18 14:27:59 -0600

    added an interface for defining rebate processors and added the fixed cash rebate processor

---
+++ Smartwyre.DeveloperTest/Services/FixedCashRebateProcessor.cs
@@ -0,0 +1,37 @@
+﻿using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Services
+{
+    /// <summary>
+    /// Code to process a fixed value rebate
+    /// </summary>
+    public class FixedCashRebateProcessor : IRebateProcessor
+    {
+        /// <inheritdoc/>
+        public decimal ProcessRebate(
+            Rebate rebate, CalculateRebateResult result, Product product)
+        {
+            // if there is no rebate, then the request fails to process and there is no monetary value, so return 0
+            if (rebate == null)
+            {
+                return 0;
+            }
+
+            // if the product does not support fixed cash amount rebates then the request fails to process and there is no monetary value, so return 0
+            if (!product.SupportedIncentives.HasFlag(SupportedIncentiveType.FixedCashAmount))
+            {
+                return 0;
+            }
+
+            // if the rebate exists but there is no value on it, then there was no successful rebate calculation and there is no monetary value, so return 0
+            if (rebate.Amount == 0)
+            {
+                return 0;
+            }
+
+            // if all of the above validation passes, then it is a valid request so mark it as such and return the value of the rebate
+            result.Success = true;
+            return rebate.Amount;
+        }
+    }
+}
---
+++ Smartwyre.DeveloperTest/Services/IRebateProcessor.cs
@@ -0,0 +1,20 @@
+﻿using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Services
+{
+    /// <summary>
+    /// Defines a rebate processor
+    /// </summary>
+    public interface IRebateProcessor
+    {
+        /// <summary>
+        /// Executes the strategy for a specific rebate
+        /// </summary>
+        /// <param name="rebate">The rebate to process</param>
+        /// <param name="result">The result of the rebate processing request.</param>
+        /// <param name="product">The product to the validity of the rebate against.</param>
+        /// <returns>The monetary value of the rebate, if any.</returns>
+        decimal ProcessRebate(
+            Rebate rebate, CalculateRebateResult result, Product product);
+    }
+}
