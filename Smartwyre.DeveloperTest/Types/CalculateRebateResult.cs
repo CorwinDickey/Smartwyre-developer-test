71e0631ccf3cefb2526b96370157d0ae85fa8a87
Corwin Dickey <62188881+CorwinDickey@users.noreply.github.com>
2025-02-18 14:20:37 -0600

    added default false to CalculateRebateResult success status

--- Smartwyre.DeveloperTest/Types/CalculateRebateResult.cs
+++ Smartwyre.DeveloperTest/Types/CalculateRebateResult.cs
@@ -1,6 +1,15 @@
 ﻿namespace Smartwyre.DeveloperTest.Types;
 
+/// <summary>
+/// Class containing the result of a rebate calculation request
+/// </summary>
 public class CalculateRebateResult
 {
-    public bool Success { get; set; }
+    /// <summary>
+    /// Flag indicating whether the rebate calculation was successful or not.
+    /// </summary>
+    /// <remarks>
+    /// Defaults to false to prevent false positives.
+    /// </remarks>
+    public bool Success { get; set; } = false;
 }
